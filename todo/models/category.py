d8fe83da5b5311cd120dbcd55ef742ba53429553
ThoratAbhi054 <thoratabhishek054@gmail.com>
2024-12-11 23:18:24 +0530

    Added user in category

--- todo/models/category.py
+++ todo/models/category.py
@@ -3,8 +3,10 @@ from django.utils.timezone import now
 
 from utils.mixins.pk_mixin import PKModelMixin
 from utils.mixins.timestampmixin import TimeStampMixin
+from django.contrib.auth.models import User
 
 
 
 class Category(PKModelMixin, TimeStampMixin, models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- todo/serializers/category.py
+++ todo/serializers/category.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from todo.models import Category
+from django.contrib.auth.models import User
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'created_at', 'updated_at']
+        fields = ['id', 'name', 'user', 'created_at', 'updated_at']
 
+
+class CategoryCreateSerializer(CategorySerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset = User.objects.all(), required =True)
